b83f51a63f44631779f7513960bf22734376f530
LeeTrent <Lee.Trent.1@gmail.com>
2018-02-18 10:26:03 -0600

    Initial commit - Establishes DB connectivity with MySQL inside project

---
+++ ContosoUniversity/Startup.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ContosoUniversity.Data;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Console.WriteLine("[Startup.ConfigureServices()] : BEGIN");
+
+            services.AddMvc();
+
+            // Database Connection Parameters
+            String connectionString = buildConnectionString();
+
+            // WRITE CONNECTION STRING TO THE LOG
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("[Startup] Connection String: " + connectionString);
+            Console.WriteLine("********************************************************************************");
+
+            // NOW THAT WE HAVE OUR CONNECTION STRING,
+            // WE CAN ESTABLISH OUR DB CONTEXT
+            services.AddDbContext<SchoolContext>
+            (
+		        opts => opts.UseMySQL(connectionString)
+		    );
+            Console.WriteLine("[Startup.ConfigureServices()] : END");
+        }
+
+
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private String buildConnectionString()
+        {
+            Console.WriteLine("[Startup.buildConnectionString()] : BEGIN");
+
+            String connectionString = null;
+            try
+            {
+                connectionString = Environment.GetEnvironmentVariable("LOCAL_CONNECTION_STRING");
+                if (connectionString == null)
+                {
+                    string vcapServices = System.Environment.GetEnvironmentVariable("VCAP_SERVICES");
+                    if (vcapServices != null)
+                    {
+                        dynamic json = JsonConvert.DeserializeObject(vcapServices);
+                        foreach (dynamic obj in json.Children())
+                        {
+                            dynamic credentials = (((JProperty)obj).Value[0] as dynamic).credentials;
+                            if (credentials != null)
+                            {
+                                string host     = credentials.host;
+                                string username = credentials.username;
+                                string password = credentials.password;
+                                string port     = credentials.port;
+                                string db_name  = credentials.db_name;
+
+                                connectionString = "Username=" + username + ";"
+                                    + "Password=" + password + ";"
+                                    + "Host=" + host + ";"
+                                    + "Port=" + port + ";"
+                                    + "Database=" + db_name + ";Pooling=true;";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in [Startup.buildConnectionString()]:");
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("[Startup.buildConnectionString()] : END");
+            return connectionString;
+        }
+    }
+}
