bbeb616399582e2d1df93c9e3af069229a16bda7
LeeTrent <Lee.Trent.1@gmail.com>
2018-02-19 08:35:40 -0600

    1. Creates 'initial' database and seeds it with data; 2. Creates 'Students' controller and views using .NET Core's 'scaffolding' engine.

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -8,13 +8,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+
 namespace ContosoUniversity
 {
     public class Program
     {
+        // public static void Main(string[] args)
+        // {
+        //     BuildWebHost(args).Run();
+        // }
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
